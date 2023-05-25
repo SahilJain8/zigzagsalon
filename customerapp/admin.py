8889a9745057425ccf47bb36a3d6ba69c9343446
pSeUdAiSZ <98685805+pSeUdAiSZ@users.noreply.github.com>
2023-05-25 22:01:39 +0530

    Delete customerapp directory

--- customerapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Branch, Appointment, Customer, StaffMember
-
-admin.site.register(Branch)
-admin.site.register(Appointment)
-admin.site.register(StaffMember)
-admin.site.register(Customer)
